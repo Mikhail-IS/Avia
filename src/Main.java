71b86b08af76ff8da54c14b818e7d464feb5f04e
Neto test <aklg-akk@mail.ru>
2025-04-22 13:29:39 +0700

    program automation

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 15000;
-        int rubles = 20;
-
-        if (ticket >= rubles) {
-            int miles = ticket / rubles;
-            System.out.println("Начислено миль: " + miles);
-        } else {
-            System.out.println("Начислено миль: 0");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
         }
     }
-}
